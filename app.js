41918e65dbd2a166b9539718ad1d116a48b4aeef
YehonatanKadosh <jacojacoj3@gmail.com>
2021-08-24 10:49:08 +0300

    update

--- app.js
+++ app.js
@@ -9,12 +9,6 @@ const app = express();
 app.use(cors({ exposedHeaders: "x-access-token" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.get("/", async (req, res, next) => {
-  let mongo = require("mongoose");
-  const { userSchema } = require("./services/mongoDB/models/userSchema");
-  let User = mongo.model("User", userSchema);
-  res.json(await User.find({}));
-});
 app.use("/users", user);
 app.use("/conversations", conversation);
 app.use("/logIn", logIn);
